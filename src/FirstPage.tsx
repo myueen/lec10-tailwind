4cab54a909a990d094fdfdd22497f7f764d2edf8
Yueen Ma <myueen@ad.unc.edu>
2025-02-07 10:44:47 -0500

    style text

--- src/FirstPage.tsx
+++ src/FirstPage.tsx
@@ -3,8 +3,8 @@ export default function FirstPage() {
 
     return (
         <>
-            <p>Hello, world!</p>
-            <div>
+            <p className=" text-2xl text-fuchsia-600">Hello, world!</p>
+            <div className="flex flex-row bg-amber-400 gap-2">
                 <p>One</p>
                 <p>Two</p>
             </div>
